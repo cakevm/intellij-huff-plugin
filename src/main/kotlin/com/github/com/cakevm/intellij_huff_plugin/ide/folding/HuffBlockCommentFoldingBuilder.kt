683e15f7c3a489d69c2bf9fd9486be188b581593
cakevm <cakevm@proton.me>
2024-11-10 18:51:42 +0100

    Fix folding

--- src/main/kotlin/com/github/com/cakevm/intellij_huff_plugin/ide/folding/HuffBlockCommentFoldingBuilder.kt
+++ src/main/kotlin/com/github/com/cakevm/intellij_huff_plugin/ide/folding/HuffBlockCommentFoldingBuilder.kt
@@ -1,16 +1,16 @@
 package com.github.com.cakevm.intellij_huff_plugin.ide.folding
 
-import com.github.com.cakevm.intellij_huff_plugin.language.psi.HuffElementTypes.COMMENT
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.elementType
+import com.intellij.psi.util.PsiTreeUtil
 
 class HuffBlockCommentFoldingBuilder : FoldingBuilderEx() {
   override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-    return root.children.filter { it.elementType == COMMENT }.map { FoldingDescriptor(it.node, it.textRange) }.toTypedArray()
+    return PsiTreeUtil.findChildrenOfType(root, PsiComment::class.java).map { FoldingDescriptor(it.node, it.textRange) }.toTypedArray()
   }
 
   override fun getPlaceholderText(node: ASTNode): String {
